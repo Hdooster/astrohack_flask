5c8eebcf0b2c9511cc03afb73217a318465aeecc
hdooster <hendrik.doosterlinck@sentiance.com>
2017-04-30 14:13:16 +0200

    here we are

--- flaskapp.py
+++ flaskapp.py
@@ -48,43 +48,44 @@ def upload_leaderboard(new_kv):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        if 'teamname' not in request.form:
-            flash('No teamname was chosen1!')
-            return redirect(request.url)
-
-        file = request.files['file']
-        teamname = request.form['teamname']
-
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-
-            # give unique filename
-            timestamp = arrow.utcnow().timestamp
-            filename = secure_filename(file.filename)[:-4] + '_' + teamname + '_' + str(timestamp) + '.csv'
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-
-            chi2_score = calculate_full_score(filepath)
-            leaderboard = read_leaderboard()
-            if teamname in leaderboard:
-                prev_chi2_score = leaderboard[teamname]
-            else:
-                prev_chi2_score = 'None'
-
-            upload_leaderboard((teamname, chi2_score))
-
+        # pass
+    #     # check if the post request has the file part
+    #     if 'file' not in request.files:
+    #         flash('No file part')
+    #         return redirect(request.url)
+    #     if 'teamname' not in request.form:
+    #         flash('No teamname was chosen1!')
+    #         return redirect(request.url)
+    #
+    #     file = request.files['file']
+    #     teamname = request.form['teamname']
+    #
+    #     # if user does not select file, browser also
+    #     # submit a empty part without filename
+    #     if file.filename == '':
+    #         flash('No selected file')
+    #         return redirect(request.url)
+    #     if file and allowed_file(file.filename):
+    #
+    #         # give unique filename
+    #         timestamp = arrow.utcnow().timestamp
+    #         filename = secure_filename(file.filename)[:-4] + '_' + teamname + '_' + str(timestamp) + '.csv'
+    #         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    #         file.save(filepath)
+    #
+    #         chi2_score = calculate_full_score(filepath)
+    #         leaderboard = read_leaderboard()
+    #         if teamname in leaderboard:
+    #             prev_chi2_score = leaderboard[teamname]
+    #         else:
+    #             prev_chi2_score = 'None'
+    #
+    #         upload_leaderboard((teamname, chi2_score))
+    #
             return redirect(url_for('upload_file',
-                                    filename=filename,
-                                    chi2_score=chi2_score,
-                                    prev_chi2_score=prev_chi2_score
+                                    # filename=filename
+                                    # chi2_score=chi2_score,
+                                    # prev_chi2_score=prev_chi2_score
                                     ))
     else:
         leaderboard = read_leaderboard()
